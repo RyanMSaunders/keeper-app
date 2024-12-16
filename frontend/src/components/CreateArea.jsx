3c8fc2318062cc5afe5b9af829e0b2d7ec822f5a
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-12-16 16:08:06 -0500

    Setup CreateArea component

--- frontend/src/components/CreateArea.jsx
+++ frontend/src/components/CreateArea.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
+const [note, setNote] = useState({title: "", content: ""});
+
+function handleChange(event) {
+  const {name, value} = event.target;
+  setNote(prevNote => {
+    return {
+      ...prevNote,
+      [name]: value
+    }
+  })
+}
 export default function CreateArea() {
   return (
     <form action="submit">
-      <input type="text" />
-      <textarea name="" id=""></textarea>
-      <button></button>
+      <input type="text" name='title' onChange={handleChange} value={note.title}/>
+      <textarea name="content" onChange={handleChange} value={note.content} rows="3"></textarea>
+      <button>Add</button>
     </form>
   )
 }
